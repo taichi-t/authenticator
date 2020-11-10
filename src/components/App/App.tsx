0363d54b6097aee7a7c8ec4ba7b6bc4413d4946c
Tsujioka Taichi <t.tujioka.0925@gmail.com>
2020-11-10 00:31:49 -0800

    :heavy_plus_sign: Add redux toolkit, react-redux and @types/react-redux

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,20 +1,17 @@
 import * as React from 'react';
-import AuthProvider from '@/lib/AuthProvider';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from '@/src/components/Home/Home';
 import Layout from '@/src/components/Layout/Layout';
 
 const App: React.FC = () => {
   return (
-    <AuthProvider>
-      <BrowserRouter>
-        <Layout>
-          <Switch>
-            <Route path="/" exact component={Home} />
-          </Switch>
-        </Layout>
-      </BrowserRouter>
-    </AuthProvider>
+    <BrowserRouter>
+      <Layout>
+        <Switch>
+          <Route path="/" exact component={Home} />
+        </Switch>
+      </Layout>
+    </BrowserRouter>
   );
 };
 
