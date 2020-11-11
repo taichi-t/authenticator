aa15a5f44744f2e6324ebba701a9ea864d2051f9
Tsujioka Taichi <t.tujioka.0925@gmail.com>
2020-11-10 19:32:20 -0800

    :fire: Remove fetchMockData

--- src/util/fetchMockData.ts
+++
@@ -1,8 +0,0 @@
-// import axios from 'axios';
-
-// export const fetchMockData = async () => {
-//   const res = await axios.get('https://jsonplaceholder.typicode.com/todos/1');
-//   return res;
-// };
-
-// export default fetchMockData;
