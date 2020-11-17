950918aa7b31404741ffa92c21c401e3ac25bcab
Tsujioka Taichi <t.tujioka.0925@gmail.com>
2020-11-17 05:54:52 -0800

    :white_check_mark: Update tests for errorHandler.

--- src/server/__tests__/errorHandler.test.ts
+++ src/server/__tests__/errorHandler.test.ts
@@ -1,7 +1,6 @@
 import { Request, Response } from 'express';
 import boom from '@hapi/boom';
 import { errorHandler } from '../middleware/error';
-
 // import request from 'supertest';
 
 describe('errorHandler', () => {
@@ -18,17 +17,27 @@ describe('errorHandler', () => {
     };
   });
 
-  it('shoud be called send fn with Error object', async (done) => {
+  it('shoud call send fn with Error object', async (done) => {
     const mockError = boom.badRequest('test error');
-    console.log(mockError);
     errorHandler(
       mockError as boom.Boom<Error>,
       mockRequest as Request,
       mockResponse as Response
     );
-
     expect(mockResponse.send).toBeCalledWith({ error: mockError });
 
+    done();
+  });
+
+  it('has no statusCode, erro object should contain 500 of statusCode', async (done) => {
+    const mockError = boom.badData('test Data', new Error());
+    mockError.output.statusCode = undefined;
+    errorHandler(
+      mockError as boom.Boom<Error>,
+      mockRequest as Request,
+      mockResponse as Response
+    );
+    expect(mockResponse.status).toBeCalledWith(500);
     done();
   });
 });
