259cf4846893f888c01956513f8eee577c688379
Tsujioka Taichi <t.tujioka.0925@gmail.com>
2020-12-04 19:22:36 -0800

    :boom: Create user store.

--- src/client/app/store.ts
+++ src/client/app/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authSliceReducer from '@/client/features/auth/authSlice';
+import userSliceReducer from '@/client/features/users/userSlice';
 
 const store = configureStore({
   reducer: {
     authStore: authSliceReducer,
+    userStore: userSliceReducer,
   },
   devTools: process.env.NODE_ENV === 'development',
 });
