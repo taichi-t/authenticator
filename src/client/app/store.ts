9f5502a0028f479243321a8da768170757dedd99
Tsujioka Taichi <t.tujioka.0925@gmail.com>
2020-12-05 16:46:35 -0800

    :sparkles: Define root reducer.

--- src/client/app/store.ts
+++ src/client/app/store.ts
@@ -1,13 +1,17 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import authSliceReducer from '@/client/features/auth/authSlice';
 import userSliceReducer from '@/client/features/users/userSlice';
 
+const rootReducer = combineReducers({
+  authStore: authSliceReducer,
+  userStore: userSliceReducer,
+});
+
 const store = configureStore({
-  reducer: {
-    authStore: authSliceReducer,
-    userStore: userSliceReducer,
-  },
+  reducer: rootReducer,
   devTools: process.env.NODE_ENV === 'development',
 });
+
+export type RootState = ReturnType<typeof rootReducer>;
 
 export default store;
