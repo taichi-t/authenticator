9cddec1a5944f81e0395496b810b94f3d1cd6b92
Tsujioka Taichi <t.tujioka.0925@gmail.com>
2020-12-06 01:40:15 -0800

    :fire: Remove useless code.

--- src/client/app/App.tsx
+++ src/client/app/App.tsx
@@ -3,17 +3,14 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from '@/client/components/Home/Home';
 import Layout from '@/client/components/Layout/Layout';
 import { authSelector, fetchAuth } from '@/client/features/auth/authSlice';
-// import { userSelector } from '@/client/features/users/userSlice';
 import { useSelector, useDispatch } from 'react-redux';
 
 const App: React.FC = () => {
   const dispatch = useDispatch();
   const { auth } = useSelector(authSelector);
-  // const { user } = useSelector(userSelector);
 
   React.useEffect(() => {
     dispatch(fetchAuth());
-    // dispatch(fetchUser());
   }, [dispatch]);
 
   console.log({ auth });
